ef70cdd509f4d912b1daca8a72ab311058777ed1
j- <jh@live.com.au>
2015-06-11 12:11:58 +1000

    Test proxy content

--- tests/unit/array-offset-test.js
+++ tests/unit/array-offset-test.js
@@ -8,3 +8,14 @@ QUnit.test('constructor exists', function (assert) {
 	assert.ok(ArrayOffset, 'ArrayOffset is not null or undefined');
 	assert.equal(typeof ArrayOffset, 'function', 'ArrayOffset is function');
 });
+
+QUnit.test('content is initialized', function (assert) {
+	var arr = Em.A(['a', 'b', 'c']);
+	var proxy = ArrayOffset.create({
+		content: arr
+	});
+	assert.equal(proxy.get('arrangedContent.length'), 3);
+	assert.equal(proxy.get('content.length'), 3);
+	assert.equal(proxy.get('length'), 3);
+	assert.equal(proxy.get('arrangedContent').join(''), 'abc');
+});
