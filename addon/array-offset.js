0b56807efb96038ce799d483e73f5be8179fb090
j- <jh@live.com.au>
2015-06-11 12:03:09 +1000

    Converted computed properties to get/set pattern

--- addon/array-offset.js
+++ addon/array-offset.js
@@ -5,43 +5,46 @@ var computed = Em.computed;
 var DEFAULT_OFFSET = 0;
 
 var ArrayOffset = Em.ArrayProxy.extend({
-	offset: computed(function (name, offset, old) {
-		// getter
-		if (arguments.length < 2) {
+	offset: computed({
+		get: function () {
 			return DEFAULT_OFFSET;
-		}
-		offset = Number(offset); // ensure offset is number
-		offset = Math.max(offset, 0); // do not allow negative offset
-		if (old === undefined) {
-			// being set for first time, no need to update
+		},
+		set: function (name, offset, old) {
+			offset = Number(offset); // ensure offset is number
+			offset = Math.max(offset, 0); // do not allow negative offset
+			if (old === undefined) {
+				// being set for first time, no need to update
+				return offset;
+			}
+			var diff = offset - old;
+			if (diff === 0) {
+				// no need to continue if no difference
+				return offset;
+			}
+			var arranged = this.get('arrangedContent');
+			// offset added, remove items
+			if (diff > 0) {
+				var arrangedLength = get(arranged, 'length');
+				var removeCount = Math.min(diff, arrangedLength);
+				arranged.replace(0, removeCount);
+			}
+			// offset removed, add items
+			else {
+				var content = this.get('content');
+				var toAdd = content.slice(offset, old);
+				arranged.replace(0, 0, toAdd);
+			}
 			return offset;
 		}
-		var diff = offset - old;
-		if (diff === 0) {
-			// no need to continue if no difference
-			return offset;
-		}
-		var arranged = this.get('arrangedContent');
-		// offset added, remove items
-		if (diff > 0) {
-			var arrangedLength = get(arranged, 'length');
-			var removeCount = Math.min(diff, arrangedLength);
-			arranged.replace(0, removeCount);
-		}
-		// offset removed, add items
-		else {
-			var content = this.get('content');
-			var toAdd = content.slice(offset, old);
-			arranged.replace(0, 0, toAdd);
-		}
-		return offset;
 	}),
 
-	arrangedContent: computed('content', function () {
-		var content = this.get('content');
-		var offset = this.get('offset');
-		var slice = content.slice(offset);
-		return Em.A(slice);
+	arrangedContent: computed('content', {
+		get: function () {
+			var content = this.get('content');
+			var offset = this.get('offset');
+			var slice = content.slice(offset);
+			return Em.A(slice);
+		}
 	}),
 
 	// process items removed
